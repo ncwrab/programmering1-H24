98ff4ae614a67bf3771f2f2d00f384ffcfa6d2b9
ncwrab <143709905+ncwrab@users.noreply.github.com>
2024-08-23 14:46:36 +0200

    Create datatyper.py

---
+++ Forelesninger/Forelesning02-variabler og datatyper/datatyper.py
@@ -0,0 +1,33 @@
+variabel = 5            # Innholdet i variabelen er av datatypen int (heltall)
+print(variabel)
+
+
+# Hvilke verdier vil bli printet ut? 
+number = 2
+print(number)
+number = 6
+print(number)
+
+# Hva vil skje her?
+x, y = 12, 34.9          # Innholdet i variabel x er av datatypen int, og innholdet i y er av datatypen float
+print(x)
+print(y)
+
+# linje 12 kan enklere og mer oversiktlig skrives over 2 linjer slik:
+#x = 12
+#y = 37.5
+
+
+
+# Hva vil skje her?
+a = 2
+b = 2
+print(a)
+a = a + b
+print(a)
+b = a + b
+print(b)
+
+# Hva blir utskriften her?
+dolphins_sleep_with_one_eye_open = False          # Innholdet i variabelen er av typen boolean
+print(dolphins_sleep_with_one_eye_open)
