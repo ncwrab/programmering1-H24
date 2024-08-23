02087b5ad9b1779bddf184f9384d761d708af51c
ncwrab <143709905+ncwrab@users.noreply.github.com>
2024-08-23 14:48:30 +0200

    Create variabler.py

---
+++ Forelesninger/Forelesning02-variabler og datatyper/variabler.py
@@ -0,0 +1,12 @@
+# Opprette og bruke variabler
+
+course_name = "Programmering 3"     # Oppretter en variabel med navnet course_name. Innholdet settes til "Programmering 3". Innholdet er av datatypen string, som kjennetegnes av "" rundt innholdet.
+print(course_name)                  # Skriver ut innholdet i variabelen til konsollen
+
+print("course_name")                # Her brukes ikke variabelen (dette blir en string fordi vi har " " rundt teksten)
+
+course_name = "Programmering 1"     # Endrer/oppdaterer/overskriver innholdet i variabelen
+print(course_name)                  # Bruker variabelen, med det nye innholdet
+
+variabel = None                     # Definerer en tom variabel
+print(variabel)
