7adca8a3b4f575551cab9bb2279c6194468e7093
ncwrab <143709905+ncwrab@users.noreply.github.com>
2024-08-31 10:12:28 +0200

    Create errors.py

---
+++ Forelesninger/Forelesning03 - datatyper og feilmeldinger/errors.py
@@ -0,0 +1,23 @@
+# Ta bort kommentarene fra kodelinjene der du har lyst til å fremprovosere en feil.
+
+# ZeroDivisionError
+#a = 42/0
+
+# NameError
+
+#sitat = 42
+#print(sitatt)
+
+# ValueError
+
+#tekst = "Dette er en tekst"
+#nummer = int(tekst)
+
+# TypeError
+
+#planeter = "Antall planeter i solsystemet: "
+#antall_planeter = 8
+#print(planeter + antall_planeter)
+
+# Ved å formatere utskriften på følgende måte ville det fungert:
+#print(f"{planeter} {antall_planeter}")
