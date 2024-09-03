41fc16657956072da4e538da95a7ba03e0669e60
ncwrab <143709905+ncwrab@users.noreply.github.com>
2024-09-03 12:59:16 +0200

    Create aritmetiske_operatorer.py

---
+++ Forelesninger/Forelesning04 - operatorer og if-tester/aritmetiske_operatorer.py
@@ -0,0 +1,31 @@
+
+# Vi kan bruke aritmetiske operatorer direkte på tall
+print( 4 * 7)
+
+# Mer vanlig er det å lagre verdier i variabler for så å bruke variablenes data i videre utregninger
+# Her opprettes to variabler med hver sin verdi.
+num_1 = 4
+num_2 = 7
+
+# Under brukes variablenes verdi for å utføre enkel matematikk med de aritmetiske operatorene
+
+answer = num_1 + num_2
+print(f"Addisjon: {answer}")
+
+answer = num_1 - num_2
+print(f"Subtraksjon: {answer}")
+
+answer = num_1 * num_2
+print(f"Multiplikasjon: {answer}")
+
+answer = num_1 / num_2
+print(f"Divisjon: {answer}")
+
+answer = num_1 % num_2
+print(f"Modulus: {answer}")
+
+answer = num_1 ** num_2
+print(f"Eksponentiering: {answer}")
+
+answer = num_1 // num_2
+print(f"Floor division: {answer}")
