261d319864f94fb9a649765e01e212dfd15f957e
ncwrab <143709905+ncwrab@users.noreply.github.com>
2024-10-14 10:46:16 +0200

    Create funksjon_dato.py

---
+++ Forelesninger/Forelesning11 - dict forts, funksjoner/funksjon_dato.py
@@ -0,0 +1,9 @@
+import datetime
+# Vi kan google 'python datetime documentation' for å lese om hvordan denne modulen fungerer
+
+
+def gi_dagens_dato():
+    dagens_dato = datetime.date.today()
+    print(dagens_dato)
+
+gi_dagens_dato()
